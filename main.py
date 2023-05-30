9ed3aee8386cc583c466fefcc3225fae58318c6c
balon41 <132463000+balon41@users.noreply.github.com>
2023-05-30 16:53:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ img = load_image()
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = EfficientNetB0(weights='imagenet')
+    model = EfficientNetB6(weights='imagenet')
     return model
 
 
