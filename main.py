82ec0b9dba9c0cbb8e5fb0f1f6031af67fd7aadb
balon14 <117996775+balon14@users.noreply.github.com>
2023-05-30 17:12:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,14 @@
 import io
 import streamlit as st
 from PIL import Image
+import numpy as np
+from tensorflow.keras.applications import EfficientNetB6
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 
 
-def load_image():
-    """Создание формы для загрузки изображения"""
-    # Форма для загрузки изображения средствами Streamlit
-    uploaded_file = st.file_uploader(
-        label='Выберите изображение для распознавания')
-    if uploaded_file is not None:
-        # Получение загруженного изображения
-        image_data = uploaded_file.getvalue()
-        # Показ загруженного изображения на Web-странице средствами Streamlit
-        st.image(image_data)
-        # Возврат изображения в формате PIL
-        return Image.open(io.BytesIO(image_data))
-    else:
-        return None
-
-    
-# Выводим заголовок страницы средствами Streamlit     
-st.title('Классификация изображений')
-# Вызываем функцию создания формы загрузки изображения
-img = load_image()
+name = st.text_input("Enter your name", "")
+st.write(f"Hello {name}!")
 
 
 @st.cache(allow_output_mutation=True)
@@ -39,12 +25,31 @@ def preprocess_image(img):
     return x
 
 
+def load_image():
+    """Создание формы для загрузки изображения"""
+    # Форма для загрузки изображения средствами Streamlit
+    uploaded_file = st.file_uploader(
+        label='Выберите изображение для распознавания')
+    if uploaded_file is not None:
+        # Получение загруженного изображения
+        image_data = uploaded_file.getvalue()
+        # Показ загруженного изображения на Web-странице средствами Streamlit
+        st.image(image_data)
+        # Возврат изображения в формате PIL
+        return Image.open(io.BytesIO(image_data))
+    else:
+        return None
+    
+    
 def print_predictions(preds):
     classes = decode_predictions(preds, top=5)[0]
     for cl in classes:
         st.write(cl[1], cl[2])
-        
-        
+
+    
+model = load_model()
+
+
 # Загружаем предварительно обученную модель
 model = load_model()
 # Выводим заголовок страницы
